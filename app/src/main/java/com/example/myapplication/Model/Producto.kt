e17c1c8391a91be2110a3d9f9616d0a682888fe3
Alexis Jean Ríos Haro <al.riosh@duocuc.cl>
2025-10-22 11:10:23 -0300

    Implementacion de productos
    Imgenes, base producto, json terminado

--- app/src/main/java/com/example/myapplication/Model/Producto.kt
+++ app/src/main/java/com/example/myapplication/Model/Producto.kt
@@ -1,4 +1,14 @@
 package com.example.myapplication.Model
 
 class Producto {
+    val id: Int,
+    val sku: String,
+    val nombre: String,
+    val categoria: String,
+    val subcategoria: String,
+    val precio: Int,
+    val enStock: Boolean,
+    val stock: Int,
+    //val imagen: String,
+    val descripcion: String
 }
