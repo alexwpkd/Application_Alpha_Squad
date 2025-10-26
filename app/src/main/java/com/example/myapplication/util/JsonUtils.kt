558fdb5f3175452cdb2372d4fb0833e1f73705c8
BenjaminGonzalez1212 <benjam.gonzalezl@duocuc.cl>
2025-10-26 20:33:33 -0300

    Revert "Actualizacion admin #2"
    This reverts commit 45af77afb37e302a6c5e34ca4a8ab0568d248711.

--- app/src/main/java/com/example/myapplication/util/JsonUtils.kt
+++ app/src/main/java/com/example/myapplication/util/JsonUtils.kt
@@ -3,6 +3,7 @@ package com.example.myapplication.util
 import android.content.Context
 import com.example.myapplication.Model.Producto
 import org.json.JSONObject
+import org.json.JSONArray
 import java.io.IOException
 
 fun loadProducts(context: Context): List<Producto> {
@@ -12,12 +13,26 @@ fun loadProducts(context: Context): List<Producto> {
         val jsonString = context.assets.open("productos.json")
             .bufferedReader().use { it.readText() }
 
-        val jsonObject = JSONObject(jsonString)
+        /*val jsonObject = JSONObject(jsonString)
+        val jsonArray = jsonObject.getJSONArray("productos")*/
+        val jsonArray = JSONArray(jsonString)
 
-        val jsonArray = jsonObject.getJSONArray("productos")
 
         for (i in 0 until jsonArray.length()) {
             val obj = jsonArray.getJSONObject(i)
+
+<<<<<<< Updated upstream
+            val nombreDrawable = obj.getString("imagenClave")
+            val idDrawable = context.resources.getIdentifier(
+                nombreDrawable,  // ej: "product_img_1"
+                "drawable",      // carpeta res/drawable
+                context.packageName
+            ).let { id ->
+                if (id != 0) id else android.R.drawable.ic_menu_report_image
+            }
+
+=======
+>>>>>>> Stashed changes
             val producto = Producto(
                 id = obj.getInt("id"),
                 sku = obj.getString("sku"),
@@ -27,7 +42,7 @@ fun loadProducts(context: Context): List<Producto> {
                 precio = obj.getInt("precio"),
                 enStock = obj.getBoolean("enStock"),
                 stock = obj.getInt("stock"),
-                imagenClave = obj.getString("imagenClave"),
+                imagenClave = idDrawable,
                 descripcion = obj.getString("descripcion")
             )
             productos.add(producto)
