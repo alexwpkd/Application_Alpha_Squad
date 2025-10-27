45a3612b2451b3f9e390f80f15deea14aece38b9
Alexis Jean Ríos Haro <al.riosh@duocuc.cl>
2025-10-27 01:00:53 -0300

    Update build.gradle.kts
    Dependencia iconos

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,6 +62,9 @@ dependencies {
     implementation(libs.androidx.compose.material3)
     debugImplementation(libs.androidx.ui.tooling)
 
+    // Íconos de Material (filled)
+    implementation("androidx.compose.material:material-icons-extended:1.6.7")
+
     // Navigation Compose
     implementation("androidx.navigation:navigation-compose:2.7.7")
 
